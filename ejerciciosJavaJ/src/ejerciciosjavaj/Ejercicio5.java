167de6407ae010f87068c5fbbc896b04b4dbf16e
Surrafa08 <109037569+Surrafa08@users.noreply.github.com>
2025-02-18 09:26:20 -0500

    5

---
+++ ejerciciosJavaJ/src/ejerciciosjavaj/Ejercicio5.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejerciciosjavaj;
+
+/**
+ *
+ * @author jaide
+ */
+public class Ejercicio5 {
+    
+}
